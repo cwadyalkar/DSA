a316a2326958db046af778357d418fa99b2faff4
Chirag Wadyalkar <98507900+cwadyalkar@users.noreply.github.com>
2025-04-29 09:23:55 +0530

    Time: 374 ms (26.49%) | Memory: 41 MB (73.83%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,40 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    public boolean solve(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    for (char ch = '1'; ch <= '9'; ch++) {
+                        if (isValid(board, row, col, ch)) {
+                            board[row][col] = ch;
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[row][col] = '.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isValid(char board[][], int row, int col, char ch) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == ch)
+                return false;
+            if (board[i][col] == ch)
+                return false;
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == ch)
+                return false;
+
+        }
+        return true;
+
+    }
+}
