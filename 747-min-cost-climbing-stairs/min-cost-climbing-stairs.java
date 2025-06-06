0947e719331bcc5b8689486e2e4b9dd2dd0aeae9
Chirag Wadyalkar <98507900+cwadyalkar@users.noreply.github.com>
2025-06-06 08:57:44 +0530

    Time: 1 ms (38.40%) | Memory: 43.2 MB (49.94%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -8,6 +8,7 @@ class Solution {
 
     public int recursion(int n, int[] cost,int dp[]) {
         if (n == 0 || n == 1) {
+            dp[n] = 0;
             return 0;
         }
         if(dp[n] != -1){
