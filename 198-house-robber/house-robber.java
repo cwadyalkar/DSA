d374f50a45c833574c52853c79361d643fd66b79
Chirag Wadyalkar <98507900+cwadyalkar@users.noreply.github.com>
2025-06-08 12:49:18 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (8.64%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,25 +1,40 @@
 class Solution {
+    // public int rob(int[] nums) {
+    //     int n = nums.length;
+    //     int dp[] = new int[n];
+    //     Arrays.fill(dp, -1);
+    //     return recursion(nums, n - 1, dp);
+    // }
+
+    // public int recursion(int[] nums, int index, int dp[]) {
+    //     if (index == 0) {
+    //         return nums[0];
+    //     }
+    //     if (index < 0) {
+    //         return 0;
+    //     }
+
+    //     if (dp[index] != -1)
+    //         return dp[index];
+
+    //     int pick = nums[index] + recursion(nums, index - 2, dp);
+    //     int noPick = 0 + recursion(nums, index - 1, dp);
+    //     dp[index] = Math.max(pick, noPick);
+    //     return dp[index];
+    // }
+
     public int rob(int[] nums) {
         int n = nums.length;
-        int dp[] = new int[n];
-        Arrays.fill(dp, -1);
-        return recursion(nums, n - 1, dp);
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int state = 2; state < n + 1; state++) {
+            int pick = nums[state - 1] + dp[state - 2];
+            int noPick = 0 + dp[state - 1];
+            dp[state] = Math.max(pick, noPick);
+
+        }
+        return dp[n];
     }
 
-    public int recursion(int[] nums, int index, int dp[]) {
-        if (index == 0) {
-            return nums[0];
-        }
-        if (index < 0) {
-            return 0;
-        }
-
-        if (dp[index] != -1)
-            return dp[index];
-
-        int pick = nums[index] + recursion(nums, index - 2, dp);
-        int noPick = 0 + recursion(nums, index - 1, dp);
-        dp[index] = Math.max(pick, noPick);
-        return dp[index];
-    }
 }
