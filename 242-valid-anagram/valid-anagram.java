180e8914cea34b7964343f4c9feca3af37cdae4e
Chirag Wadyalkar <98507900+cwadyalkar@users.noreply.github.com>
2026-02-01 19:23:26 +0530

    Time: 16 ms (21.74%) | Memory: 46.4 MB (39.42%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,24 +1,24 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch1 = s.charAt(i);
-            char ch2 = t.charAt(i);
-            System.out.println(ch1);
-            System.out.println(ch2);
-            map.put(ch1,map.getOrDefault(ch1,0)+1);
-                        System.out.println(map);
-
-            map.put(ch2,map.getOrDefault(ch2,0)-1);
-            
-
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i = 0;i < s.length();i++){
+            char sCh = s.charAt(i);
+            map.put(sCh,map.getOrDefault(sCh,0)+1);            
         }
-        for(int ele : map.values()){
-                if(ele != 0){
-                    return false;
-                }
+
+        for(int i = 0;i < t.length();i++){
+            char tCh = t.charAt(i);
+            map.put(tCh,map.getOrDefault(tCh,0)-1);            
+        }
+
+        for(int value : map.values()){
+            if(value > 0){
+                return false;
             }
+        }
+
         return true;
+
     }
 }
