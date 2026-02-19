d3ad1b36249a321207a9999c2f745a84a5add260
Chirag Wadyalkar <98507900+cwadyalkar@users.noreply.github.com>
2026-02-19 23:04:23 +0530

    Time: 11 ms (37.10%) | Memory: 65.3 MB (5.86%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -10,54 +10,46 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null) {
+      return mergeSort(head);
+    }
+    public ListNode mergeSort(ListNode head){
+        if(head == null || head.next == null){
             return head;
         }
-
-        ListNode mid = getMid(head);
-        ListNode right = mid.next;
-        mid.next = null;
-        ListNode left = head;
-
-        ListNode leftMid = sortList(left);
-        ListNode rightMid = sortList(right);
-
-        return mergeList(leftMid, rightMid);
-
-    }
-
-    public ListNode mergeList(ListNode list1, ListNode list2) {
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-        ListNode l1 = list1;
-        ListNode l2 = list2;
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                curr.next = l1;
-                l1 = l1.next;
-            } else {
-                curr.next = l2;
-                l2 = l2.next;
-            }
-            curr = curr.next;
-        }
-        if (l1 != null) {
-            curr.next = l1;
-        }
-        if (l2 != null) {
-            curr.next = l2;
-        }
-
-        return dummy.next;
-    }
-
-    public ListNode getMid(ListNode head) {
         ListNode slow = head;
-        ListNode fast = head.next;
-        while (fast != null && fast.next != null) {
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while(fast != null && fast.next != null){
+            prev = slow;
             slow = slow.next;
             fast = fast.next.next;
         }
-        return slow;
+        prev.next = null;
+        ListNode head1 = mergeSort(head);
+        ListNode head2 = mergeSort(slow);
+        ListNode ans = merge(head1,head2);
+        return ans;
+    }
+    public ListNode merge(ListNode head1,ListNode head2){
+        ListNode ansNode = new ListNode(-1);
+        ListNode ansTail = ansNode;
+        while(head1 != null && head2 != null){
+            if(head1.val <= head2.val){
+                ansTail.next = new ListNode(head1.val);
+                head1 = head1.next;
+            }else{
+               ansTail.next = new ListNode(head2.val);
+                head2 = head2.next;  
+            }
+            ansTail = ansTail.next;
+        }
+         if (head1 != null) {
+            ansTail.next = head1;
+        } else {
+            ansTail.next = head2;
+        }
+
+        return ansNode.next;
     }
 }
