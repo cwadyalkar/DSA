f7cced3b7af30279031ed3baf09c8b55f4e01ea0
Chirag Wadyalkar <98507900+cwadyalkar@users.noreply.github.com>
2026-02-24 14:08:53 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (49.73%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -4,13 +4,12 @@ class Solution {
         int end = nums.length - 1;
         while(start <= end){
             int mid = start + (end - start)/2;
-            System.out.println(nums[mid]);
             if(nums[mid] == target){
                 return mid;
-            }else if(nums[mid] > target){
-                end = mid - 1;
+            }else if(nums[mid] < target){
+                start = mid + 1;
             }else{
-                start = start + 1;
+                end = mid - 1;
             }
         }
         return -1;
