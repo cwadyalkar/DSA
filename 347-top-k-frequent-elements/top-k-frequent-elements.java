99beeeb0191c9dc233dafd9c71ec5d32473cb18e
Chirag Wadyalkar <98507900+cwadyalkar@users.noreply.github.com>
2026-02-26 07:08:54 +0530

    Time: 43 ms (5.00%) | Memory: 48 MB (14.58%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,47 +1,24 @@
 class Solution {
-    class Number implements Comparable<Number> {
-        int freq;
-        int element;
-
-        Number(int freq, int element) {
-            this.freq = freq;
-            this.element = element;
-        }
-
-        public int compareTo(Number that) {
-            return that.freq - this.freq;
-
-        }
-    }
-
     public int[] topKFrequent(int[] nums, int k) {
-        // PriorityQueue (Min-Heap) to store top k frequent elements
-        PriorityQueue<Number> pq = new PriorityQueue<>();
-
-        // HashMap to store the frequency of each number
-        HashMap<Integer, Integer> freqMap = new HashMap<>();
-        for (int num : nums) {
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int ans[] = new int[k];
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0)+1);
         }
-        // Insert elements into the priority queue based on frequency
-
-        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
-            Number number = new Number(entry.getValue(), entry.getKey());
-            pq.offer(number);
+        for(int i = 0;i < ans.length;i++){
+            int maxValue = Integer.MIN_VALUE;
+            int maxKey = 0;
+            for(Map.Entry<Integer,Integer> entry : map.entrySet()){ 
+                if(entry.getValue() > maxValue){
+                    maxKey = entry.getKey();
+                    maxValue = entry.getValue();
+                }
+            }
+            ans[i] = maxKey;
+            map.remove(maxKey);
+            
         }
-        // Array to store the result
-        int res[] = new int[k];
-        int index = 0;
-
-        // Extract top k frequent elements from the priority queue
-
-        while (index < k) {
-            Number number = pq.poll();
-            res[index] = number.element;
-            index++;
-        }
-        return res;
-
-        // i should revise once
+      
+        return ans;
     }
 }
