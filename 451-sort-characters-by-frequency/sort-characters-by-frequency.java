9401ecfa264338bed24ca1fb4a69a4f4bd03fd7f
Chirag Wadyalkar <98507900+cwadyalkar@users.noreply.github.com>
2026-02-26 08:20:30 +0530

    Time: 20 ms (17.86%) | Memory: 48.4 MB (5.71%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,23 +1,53 @@
 class Solution {
+    class CharFrequency implements Comparable<CharFrequency>{
+        char ch;
+        int frequency;
+
+        CharFrequency(char ch,int frequency){
+                this.ch = ch;
+                this.frequency = frequency;
+        }
+
+        public int compareTo(CharFrequency that){
+            return that.frequency - this.frequency;
+        }
+    
+        public String toString() {
+            return "(" + ch + ", " + frequency + ")";
+        }
+
+    }
     public String frequencySort(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (char ch : s.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
-        PriorityQueue<Character> maxHeap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
-        maxHeap.addAll(map.keySet());
+        HashMap<Character,Integer> map = new HashMap<>();
+        PriorityQueue<CharFrequency> pq = new PriorityQueue<>();
         StringBuilder sb = new StringBuilder();
-        while (!maxHeap.isEmpty()) {
-            char ch = maxHeap.poll();
-            sb.append(String.valueOf(ch).repeat(map.get(ch)));
+        for(char ch : s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        // System.out.println("map--->"+map);
+
+        
+        for(Map.Entry<Character,Integer> entry : map.entrySet()){
+            CharFrequency charFrequency = new CharFrequency(entry.getKey(),entry.getValue());
+            
+            pq.offer(charFrequency);
+        }
+        System.out.println("pq--->"+pq);
+
+        
+
+      while(!pq.isEmpty()){
+            CharFrequency charFrequencies = pq.poll();
+            char ch = charFrequencies.ch;
+            int frequency = charFrequencies.frequency;
+            System.out.println("ch--->"+ch);
+            System.out.println("frequency--->"+frequency);
+
+            for(int j = 0;j < frequency;j++){
+                sb.append(ch);
+            }
 
         }
-
         return sb.toString();
     }
 }
-
-
-
-// Revise It Once
-// uses Comparable here for ehap sorting
