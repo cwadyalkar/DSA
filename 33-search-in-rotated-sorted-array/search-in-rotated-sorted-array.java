dcda117416d5a268e589ee6dbcfd9420c299eb23
Chirag Wadyalkar <98507900+cwadyalkar@users.noreply.github.com>
2026-03-01 10:43:35 +0530

    Time: 11 ms (1.50%) | Memory: 44 MB (26.52%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,27 +1,30 @@
 class Solution {
     public int search(int[] nums, int target) {
         int start = 0;
-        int end = nums.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] == target) {
-                return mid;
+        int end = nums.length -1;
+        return findRecursively(nums,target,start,end);
+    }
+    public int findRecursively(int nums[],int target,int start,int end){
+        int mid = start + (end - start)/2;
+        System.out.println(mid);
+        if(start > end) return -1;
+         System.out.println("start"+start+" "+"end"+end);
+
+        if(nums[mid] == target) return mid;
+        if(nums[start] <= nums[mid]){
+            if(target >= nums[start] && target < nums[mid]){
+                return findRecursively(nums,target,start,mid-1);
+            }else{
+                return findRecursively(nums,target,mid+1,end);
             }
 
-            if (nums[start] <= nums[mid]) {
-                if (target >= nums[start] && target <= nums[mid]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-            } else {
-                if (target >= nums[mid] && target <= nums[end]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
+        }else{
+            if(target > nums[mid] && target <= nums[end]){
+                return findRecursively(nums,target,mid+1,end);
+            }else{
+                return findRecursively(nums,target,start,mid-1);
             }
+
         }
-        return -1;
     }
 }
