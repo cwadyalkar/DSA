78fa9c04f881528941feeaac8c05c47c664be109
Chirag Wadyalkar <98507900+cwadyalkar@users.noreply.github.com>
2026-03-04 14:58:27 +0530

    Time: 6 ms (99.75%) | Memory: 46.6 MB (49.30%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.java
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -8,31 +8,36 @@ class Solution {
             this.frequency = frequency;
         }
 
-       @Override
-        public int compareTo(Words that) {
+         public int compareTo(Words that) {
             if (this.frequency == that.frequency) {
                 return this.word.compareTo(that.word);
             }
             return that.frequency - this.frequency;
         }
+
+
     }
     public List<String> topKFrequent(String[] words, int k) {
-        ArrayList<String> list = new ArrayList<>();
         HashMap<String,Integer> map = new HashMap<>();
         PriorityQueue<Words> pq = new PriorityQueue<>();
         for(String str : words){
             map.put(str,map.getOrDefault(str,0)+1);
         }
 
+
         for(Map.Entry<String,Integer> entry : map.entrySet()){
-           pq.add(new Words(entry.getKey(), entry.getValue()));
-        }
-        for(int i = 0;i < k;i++){
-            Words mapwords = pq.poll();
-            list.add(mapwords.word);
+            Words newWord = new Words(entry.getKey(),entry.getValue());
+            pq.offer(newWord);
         }
 
-        System.out.println(map);
+        ArrayList<String> list = new ArrayList<>();
+
+        for(int i = 0;i < k;i++){
+            Words word = pq.poll();
+            list.add(word.word);
+        }
+
+
         return list;
     }
 }
