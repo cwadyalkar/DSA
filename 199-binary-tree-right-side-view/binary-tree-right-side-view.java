5c052d745c0fe6a797e3ce9f007713dacd8b74ba
Chirag Wadyalkar <98507900+cwadyalkar@users.noreply.github.com>
2026-03-05 08:52:31 +0530

    Time: 1 ms (71.33%) | Memory: 43.8 MB (49.65%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,40 +1,37 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int data;
+ *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int val) { data = val; left = null, right = null }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
- **/
-
+ */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        ArrayList<Integer> list = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
+        List<Integer> list = new ArrayList<>();
         if(root == null) return list;
-
-        while (!queue.isEmpty()) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
             int size = queue.size();
-            while (size > 0) {
-                TreeNode currNode = queue.poll();
+            for(int i = 0;i < size;i++){
+                TreeNode node = queue.poll();
+                if(i == size - 1){
+                    list.add(node.val);
+                }
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
 
-                if (currNode.left != null) {
-                    queue.offer(currNode.left);
-                }
-                if (currNode.right != null) {
-                    queue.offer(currNode.right);
-                }
-                size--;
-                if (size == 0) {
-                    list.add(currNode.val);
-
-                }
             }
-
         }
-        return list;
 
+        return list;
     }
 }
