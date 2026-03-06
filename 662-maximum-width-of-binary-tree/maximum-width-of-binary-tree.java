68cf4187234fee9877851c93dac18c3e97107a02
Chirag Wadyalkar <98507900+cwadyalkar@users.noreply.github.com>
2026-03-06 22:24:09 +0530

    Time: 1 ms (99.65%) | Memory: 45.3 MB (45.12%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -1,59 +1,63 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    static class Pair {
+    class Pair {
         TreeNode node;
         int index;
 
-        Pair(TreeNode node,int index) {
+        Pair(TreeNode node, int index) {
             this.node = node;
-             this.index = index;
+            this.index = index;
         }
     }
 
     public int widthOfBinaryTree(TreeNode root) {
-        if(root == null) return 0;
-        Queue<Pair> queue = new LinkedList<>();
         int maxWidth = 0;
-        queue.offer(new Pair(root,0));
-
-
-        while(!queue.isEmpty()){
-                int size = queue.size();
-                int firstIndex = queue.peek().index;
-                int lastIndex = firstIndex;
+        Queue<Pair> queue = new LinkedList<>();
+        queue.offer(new Pair(root, 0));
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int firstIndex = queue.peek().index;
+            int lastIndex = firstIndex;
                 
-
-            for(int i = 0;i < size;i++){
-                Pair current = queue.poll();
-                TreeNode node = current.node;
-                int index = current.index - firstIndex;
+            for (int i = 0; i < size; i++) {
+                Pair pair = queue.poll();
+                TreeNode curr = pair.node;
+                int index = pair.index;
+               
+                if(i == 0){
+                    firstIndex = index;
+                }
 
                 if(i == size - 1){
                     lastIndex = index;
                 }
-                if(node.left != null){
-                queue.offer(new Pair(node.left,2 * index));
+
+
+                if(curr.left != null){
+                    queue.offer(new Pair(curr.left,2 * index + 1));
                 }
-                if(node.right != null){
-                    queue.offer(new Pair(node.right,2 * index + 1));
+                 if(curr.right != null){
+                    queue.offer(new Pair(curr.right,2 * index + 2));
                 }
+
             }
-            maxWidth = Math.max(maxWidth,lastIndex+1);
+            maxWidth = Math.max(maxWidth,lastIndex - firstIndex + 1);
         }
+
         return maxWidth;
     }
 }
