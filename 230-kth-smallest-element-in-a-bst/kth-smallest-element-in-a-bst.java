88eeae88146bd3ad77cb5f39c1468514272bc6f9
Chirag Wadyalkar <98507900+cwadyalkar@users.noreply.github.com>
2026-03-06 15:45:01 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (80.85%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -1,45 +1,38 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-       int pos;
+    int count = 0;
+    int ans = 0;
+
     public int kthSmallest(TreeNode root, int k) {
-        TreeNode node = inorder(root, k);
-        if(node == null){
-            return -1;
-        } 
-        return node.val;
+        helper(root,k);
+        return ans;
+        
+
     }
 
-    public TreeNode inorder(TreeNode root,int k){
-     
-            if(root == null){
-                return root;
-            }
+    public void helper(TreeNode root, int k) {
+        if(root == null) return;
+        helper(root.left, k);
+        count++;
+        if(count == k){
+            ans = root.val;
+            return;
+        }
 
-            TreeNode left = inorder(root.left,k);
-            if(left != null){
-                return left;
-            }
-            pos++;
-            if(pos == k){
-                return root;
-            }
-            return inorder(root.right,k);
-           
-          
-            
+        helper(root.right, k);    
     }
 }
