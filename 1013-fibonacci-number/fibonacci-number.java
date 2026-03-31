b32b8aa9559b1339956e14c28f250a9eb6fe1756
Chirag Wadyalkar <98507900+cwadyalkar@users.noreply.github.com>
2026-03-31 10:09:18 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (89.81%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,9 +1,23 @@
 class Solution {
     public int fib(int n) {
-     if(n == 0 || n == 1){
-        return n;
-     }
-     return fib(n-1) + fib(n-2);
+        // using DP
+        int dp[] = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i] = -1;
+        }
+        return find(n, dp);
 
+    }
+
+    public int find(int n, int dp[]) {
+        if (n <= 1) {
+            return n;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        dp[n] = find(n - 1, dp) + find(n - 2, dp);
+        return dp[n];
     }
 }
