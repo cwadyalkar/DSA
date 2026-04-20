938988d3a834f556b877d4286134cfb263dd9bbd
Chirag Wadyalkar <98507900+cwadyalkar@users.noreply.github.com>
2026-04-20 10:15:35 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (69.84%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -1,30 +1,14 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    int sum  = 0;
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        return dfs(root,targetSum);
-    }
-    public boolean dfs(TreeNode root,int targetSum){
         if(root == null) return false;
-        sum = sum + root.val;
-        if(root.left  == null && root.right == null && sum == targetSum) return true;
-        boolean left = dfs(root.left,targetSum);
-        boolean right = dfs(root.right,targetSum);
-        sum = sum - root.val;
-        return left || right;
+      if(root.left == null && root.right == null && root.val == targetSum) return true;
+      int remaining = targetSum - root.val;
+      boolean leftSubtree =  hasPathSum(root.left,remaining);
+      boolean rightSubtree =  hasPathSum(root.right,remaining);   
+      if(leftSubtree || rightSubtree){
+        return true;
+      }  else{
+        return false;
+      }  
     }
 }
