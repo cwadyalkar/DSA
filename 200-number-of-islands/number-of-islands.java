ac7ed174875c7b4440c6935263fa266aff6d4361
Chirag Wadyalkar <98507900+cwadyalkar@users.noreply.github.com>
2026-04-29 11:04:39 +0530

    Time: 3 ms (86.76%) | Memory: 52.5 MB (34.40%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,37 +1,32 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0)
-            return 0;
-        int count = 0;
-        int rows = grid.length;
-        int cols = grid[0].length;
+    int island = 0;
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == '1') {
-                    dfs(grid, i, j);
-                    count++;
+    public int numIslands(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean visited[][] = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(grid[i][j] == '1' && visited[i][j] == false){
+                    dfs(grid,visited,i,j);
+                    island++;
                 }
             }
-
         }
-        return count;
+        return island;
 
     }
 
-    public void dfs(char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) {
+    public void dfs(char[][] grid, boolean visited[][], int row, int col) {
+        if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == '0'
+                || visited[row][col] == true) {
             return;
         }
-        if (grid[i][j] == '0') {
-            return;
-        }
-
-        grid[i][j] = '0';
-        dfs(grid, i + 1, j);
-        dfs(grid, i - 1, j);
-        dfs(grid, i, j + 1);
-        dfs(grid, i, j - 1);
+        visited[row][col] = true;
+        dfs(grid, visited, row - 1, col);
+        dfs(grid, visited, row, col + 1);
+        dfs(grid, visited, row + 1, col);
+        dfs(grid, visited, row, col - 1);
 
     }
 }
